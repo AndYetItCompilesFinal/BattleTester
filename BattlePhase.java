79d308b704a12b608c41b378419a63b93e3d9718
mkfloyd93 <mkfloyd93@yahoo.com>
2015-03-10 11:49:57 -0700

    Update BattlePhase.java

--- BattlePhase.java
+++ BattlePhase.java
@@ -3,74 +3,91 @@ import java.util.Scanner;
 
 public class BattlePhase {
    //Sets up turn order and sets attack into motion
-   public static boolean battle(Party good, BadGuy[] bad) {
-      BattleSetup order = new BattleSetup();
+   public Character[] a;
+   public BadGuy[] bad;
+   BattleSetup order;
+
+   
+   public boolean battle(Party good, BadGuy[] bad) 
+   {
+      order = new BattleSetup();
+      this.bad=bad;
       //add characters to order based on speed stat
-      Character[] a = {good.getParty(0), good.getParty(1), good.getParty(2), bad[0], bad[1], bad[2]};
-      
-      for (int i = 0; i < a.length-1; i++) {
+      this.a =new Character[]{good.getParty(0), good.getParty(1), good.getParty(2), bad[0], bad[1], bad[2]};
+      //selection sort
+      sort();
+      for (Character c : a) 
+      {
+         order.addChar(c);
+      }
+   
+      boolean win = round(order, good, bad);
+      if(win)
+      {
+         System.out.println("Your party won the fight!");
+         return true;
+      }
+      else{
+         System.out.println("Your party lost the fight!");
+         return false;
+      }
+   }//end of constructor
+   
+   
+   public boolean battle(Party good, BadGuy bad)
+   {
+      battle(good,new BadGuy[]{bad});
+      return false;     
+   }
+   
+   
+   public void sort()
+   {
+      for (int i = 0; i < a.length-1; i++) 
+      {
          int index = i;
-         for (int j = i + 1; j < a.length; j++){
-            if(a[j].getSpeed() > a[index].getSpeed()){
+         for (int j = i + 1; j < a.length; j++)
+         {
+            if(a[j].getSpeed() > a[index].getSpeed())
+            {
                index = j;
             }
          }
-         if(index != i){
+         if(index != i)
+         {
             Character temp = a[i];
             a[i] = a[index];
             a[index] = temp;
          }
       }
-
-      for (Character c : a) {
-         order.addChar(c);
-      }
-
-      boolean win = round(order, good, bad);
-      if(win){
-         System.out.println("Your team won the fight!");
-         return true;
-      }else{
-         System.out.println("Your team lost the fight!");
-         return false;
-      }
+      //end of sort
    }
 
    //Continues to go through turn order and runs turn operations
-   public static boolean round(BattleSetup order, Party good, BadGuy[] bad) {
-      boolean survivedTurn;
-      while(good.partyAlive() && !victory(bad)){
-         for (Object o : order) {
+   public boolean round(BattleSetup order, Party good, BadGuy[] bad) 
+   {
+      while(good.partyAlive() && !victory(bad))
+      {
+         for (Object o : order) 
+         {
             Character c = (Character)o;
-            if(c.isAlive()){
+            if(c.isAlive())
+            {
                System.out.println(c.toString() + "'s turn!");
-               if (c.isGood()) {
-                  int choice = displayMenu();
-                  if (choice == 1) {
-                     int baseAttack = ((GoodGuy)c).chooseAttack();
-                     int dmg = damage(c.getStrength(), baseAttack);
-                     int index = chooseTarget(c, bad);
-                     if(bad[index].dodgeAttempt()){
-                        System.out.println("The attack missed.");
-                     }else{
-                        System.out.println("The attack hit for " + dmg +".");
-                        boolean alive = bad[index].applyDamage(dmg);
-                        if (!alive){
-                           if(victory(bad)){
-                              return true;
-                           }
-                        }
-                     }
-                  } else {
-                     //pull up the backpack to use the item
-                     System.out.println("Access the backpack items here.");
+               //if good guy
+               if (c instanceof GoodGuy) 
+               {
+                  if(displayMenu((GoodGuy)c))
+                  {
+                     return true;
                   }
-               } else {
-                  survivedTurn = villanTurn(c, good);
-                  if(!survivedTurn){
-                    if(!good.partyAlive()){
-                       return false;
-                    }
+               } 
+               //if badguy
+               else {
+                  if(!villanTurn(c, good)){
+                     if(!good.partyAlive()){
+                        return false;
+                     }
                   }
                }
             }
@@ -80,40 +97,77 @@ public class BattlePhase {
    }
 
    //Calculates a random damage in a range
-   public static int damage(int str, int base) {
+   public int damage(int str, int base) {
       int dmg = base;
       for (int i = 1; i <= str; i++) {
-         dmg = dmg + ((int) (Math.random() * (6 - 1) + 1));
+         dmg+=((int) ((Math.random() * 5) + 1));
       }
-      dmg = dmg + base;
+      dmg+=base;
       return dmg;
    }
 
    //Lets user pick to attack or use an item
-   public static int displayMenu() {
+   public boolean displayMenu(GoodGuy c) 
+   {
+      int choice;
       Scanner kb = new Scanner(System.in);
-      System.out.println("Do you want to: ");
-      System.out.println("1. Attack");
-      System.out.println("2. Item");
-      System.out.println("Enter your choice: ");
-      int choice = kb.nextInt();
-      return choice;
+      do{
+         System.out.println("Do you want to: ");
+         System.out.println("1. Attack");
+         System.out.println("2. Item");
+         System.out.println("Enter your choice: ");
+         choice = kb.nextInt();
+         if(choice<1||choice>2)
+         {
+            System.out.println("Invalid menu choice");
+         }
+      
+      }while(choice<1||choice>2);
+      if (choice == 1) 
+      {
+         int baseAttack = ((GoodGuy)c).chooseAttack();
+         int dmg = damage(c.getStrength(), baseAttack);
+         int index = chooseTarget(c, bad);
+         if(bad[index].dodgeAttempt())
+         {
+            System.out.println("The attack missed.");
+         }
+         else
+         {
+            System.out.println("The attack hit for " + dmg +".");
+            boolean alive = bad[index].applyDamage(dmg);
+            if (!alive)
+            {
+               if(victory(bad))
+               {
+                  return true;
+               }
+            }
+         }
+      }
+      else 
+      {
+                     //pull up the backpack to use the item
+         System.out.println("Access the backpack items here.");
+      }
+      return false;
    }
 
    //Automatically runs through the villan's turn, returning whether the hero survived the attack
-   public static boolean villanTurn(Character c, Party good) {
+   public boolean villanTurn(Character c, Party good) {
       int baseAttack;
       int damage;
       baseAttack = c.chooseAttack();
       damage = damage(c.getStrength(), baseAttack);
       // choose hero to attack
-      int choice = ((int)(Math.random() * ((3 - 1)+1)));
+      int choice = ((int)(Math.random() * 3));
       boolean survived = true;
       do {
          if (good.getParty(choice).isAlive()) {
             if (good.getParty(choice).dodgeAttempt()) {
                System.out.println("The attack missed.");
-            } else {
+            } 
+            else {
                System.out.println("The attack hit for " + damage + ".");
                survived = good.getParty(choice).applyDamage(damage);
             }
@@ -123,7 +177,7 @@ public class BattlePhase {
    }
 
    //Lets the user choose the target to attack
-   public static int chooseTarget(Character c, BadGuy[] bad){
+   public int chooseTarget(Character c, BadGuy[] bad){
       int target = 0;
       while(target < 1 || target > 3) {
          System.out.println("Who do you want to attack?");
@@ -145,8 +199,10 @@ public class BattlePhase {
    }
 
    //returns whether the good guys won or not
-   public static boolean victory(BadGuy[] bad){
-      if(!bad[0].isAlive() && !bad[1].isAlive() && !bad[2].isAlive()){
+   public boolean victory(BadGuy[] bad)
+   {
+      if(!bad[0].isAlive() && !bad[1].isAlive() && !bad[2].isAlive())
+      {
          System.out.println("Minion 1: " + bad[0].getStrength());
          return true;
       }
